d5157e1bd971e1a45f3ff4d4cd35c500626655ec
Ayush-Pandey <ayush2006pandey11@gmail.com>
2026-02-09 13:52:53 +0530

    Configure base URL in vite.config.ts
    Set base URL for GitHub Pages deployment.

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      base: '/reque/', // 👈 REQUIRED for GitHub Pages
+
       server: {
         port: 3000,
         host: '0.0.0.0',
